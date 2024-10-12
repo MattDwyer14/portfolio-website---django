33373bea336fab2006cfbd2f6b1a58e5e250fc5f
MattDwyer14 <mattyd799@gmail.com>
2024-10-12 17:12:06 +0100

    added the links back and created views and url paths for the new pages

--- portfolio/core/views.py
+++ portfolio/core/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'core/home.html')
+
+def education(request):
+    return render(request, 'core/education.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
