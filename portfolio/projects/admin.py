37dad0129157323ce55e335b16c72b100376a643
MattDwyer14 <mattyd799@gmail.com>
2024-10-25 21:06:28 +0100

    compledted models fro projects and uploaded base technologies and types using shell script

--- portfolio/projects/admin.py
+++ portfolio/projects/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import TechnologyType, Technology, Project
 
-# Register your models here.
+@admin.register(TechnologyType)
+class TechnologyTypeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+@admin.register(Technology)
+class TechnologyAdmin(admin.ModelAdmin):
+    list_display = ("name", "type")
+    list_filter = ("type",)
+    search_fields = ("name", "type__name")
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "created", "updated", "technology_list")
+    list_filter = ("created", "updated", "technologies__type")
+    search_fields = ("title", "description", "technologies__name")
+    prepopulated_fields = {"slug": ("title",)}
+    ordering = ("-created",)
+    readonly_fields = ("created", "updated")
+    
+    def technology_list(self, obj):
+        return obj.technology_list()
+    technology_list.short_description = "Technologies Used"
--- portfolio/projects/models.py
+++ portfolio/projects/models.py
@@ -1,25 +1,53 @@
 from django.db import models
-
+from django.utils.text import slugify
 
 class TechnologyType(models.Model):
-    name = models.CharField(max_length=50)  # e.g., "Programming Language", "Cloud Technology"
+    name = models.CharField(max_length=50, unique=True, verbose_name="Technology Type")
 
     def __str__(self):
         return self.name
-    
+
 class Technology(models.Model):
-    name = models.CharField(max_length=100)  # e.g., "Python", "Azure"
-    type = models.ForeignKey(TechnologyType, on_delete=models.CASCADE, related_name="technologies")
+    name = models.CharField(max_length=100, unique=True, verbose_name="Technology Name")
+    type = models.ForeignKey(
+        TechnologyType,
+        on_delete=models.CASCADE,
+        related_name="technologies",
+        verbose_name="Type"
+    )
 
     def __str__(self):
         return f"{self.name} ({self.type.name})"
 
 class Project(models.Model):
-    title = models.CharField(max_length=200)
-    link = models.URLField()
-    description = models.TextField()
-    image = models.ImageField(upload_to='images/', blank=True, null=True)
-    technologies = models.ManyToManyField(Technology, related_name="projects")
+    title = models.CharField(max_length=200, verbose_name="Project Title")
+    slug = models.SlugField(max_length=250, unique=True, blank=True, verbose_name="URL Slug")
+    link = models.URLField(help_text="Enter a link to the project or repository.")
+    description = models.TextField(verbose_name="Project Description")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Date Created")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Last Updated")
+    image = models.ImageField(
+        upload_to='images/',
+        blank=True,
+        null=True,
+        help_text="Upload an image for the project.",
+        verbose_name="Project Image"
+    )
+    technologies = models.ManyToManyField(Technology, related_name="projects", verbose_name="Technologies Used")
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def technology_list(self):
+        """Returns a comma-separated list of technologies used in this project."""
+        return ", ".join(tech.name for tech in self.technologies.all())
+
+    class Meta:
+        ordering = ["-created"]
+        verbose_name = "Project"
+        verbose_name_plural = "Projects"
