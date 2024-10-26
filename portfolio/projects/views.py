86b9e50e84a246869f0642de2926e870d60f991d
MattDwyer14 <mattyd799@gmail.com>
2024-10-26 21:01:44 +0100

    order projects by msot recent first. more detailed filtering tomorrow

--- portfolio/projects/views.py
+++ portfolio/projects/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Project
 
 def project_list(request):
-    projects = Project.objects.all()
+    projects = Project.objects.all().order_by('-created')
     return render(request, 'projects/list.html', {'projects': projects})
 
 def project_detail(request, slug):
