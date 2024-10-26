f12e99dc2fcf50780e4b729b16f751e6244f67c4
MattDwyer14 <mattyd799@gmail.com>
2024-10-26 19:39:45 +0100

    styling of projects list page. further detail now needed for details page

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -1,9 +1,12 @@
 #portfolio/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('projects/', include(('projects.urls', 'projects'), namespace='projects'))
-]
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
