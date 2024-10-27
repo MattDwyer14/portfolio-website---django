13d2c16bfcc252e1231461fc44e7f763dbe39484
MattDwyer14 <mattyd799@gmail.com>
2024-10-27 19:01:13 +0000

    added filtering to projects page so users can filter by technology used in project. styled as dropdown checkbox so multiple can be selected and also added rest button to show all projects again

--- portfolio/projects/views.py
+++ portfolio/projects/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Project
+from django.db.models import Count
+from .models import Project, Technology
 
 def project_list(request):
-    projects = Project.objects.all().order_by('-created')
-    return render(request, 'projects/list.html', {'projects': projects})
+    # Get technologies used in projects and count usage
+    technologies = Technology.objects.filter(projects__isnull=False) \
+                                      .annotate(project_count=Count('projects')) \
+                                      .order_by('name')
+    
+    selected_techs = request.GET.getlist('technologies')
+    
+    projects = Project.objects.all()
+    if selected_techs:
+        projects = projects.filter(technologies__name__in=selected_techs).distinct()
+    
+    context = {
+        'projects': projects,
+        'technologies': technologies,
+        'selected_techs': selected_techs
+    }
+    return render(request, 'projects/list.html', context)
+
 
 def project_detail(request, slug):
     project = get_object_or_404(Project, slug=slug)
