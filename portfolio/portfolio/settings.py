1d81ba8f3f8f12ec1e29a7941c3ed73f84f53787
MattDwyer14 <mattyd799@gmail.com>
2024-11-06 21:15:33 +0000

    broke something, not sure what. now fixed..

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -108,11 +108,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'core/static', 
-    BASE_DIR/ 'projects/static',
+    os.path.join(BASE_DIR, 'core/static'),
+    os.path.join(BASE_DIR, 'projects/static'),
 ]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
