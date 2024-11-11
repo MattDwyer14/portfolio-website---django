e328e834906ad3a1e0ace348c11eee8cb9e58fee
MattDwyer14 <mattyd799@gmail.com>
2024-11-11 17:48:43 +0000

    removed staticfiles that arent yet needed and fixed font size bug

--- portfolio/staticfiles/projects/js/script.js
+++
@@ -1,4 +0,0 @@
-function toggleDropdown() {
-    const dropdown = document.getElementById("dropdown-container");
-    dropdown.style.display = dropdown.style.display === "none" ? "block" : "none";
-}
