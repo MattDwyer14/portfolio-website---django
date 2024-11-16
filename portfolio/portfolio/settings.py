6a34a3ba337a53e32c7b5e279f37314a88bcc1df
MattDwyer14 <mattyd799@gmail.com>
2024-11-16 18:25:20 +0000

    setup email config

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -124,8 +124,19 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 #SMTP Server
 
-#EMAIL_HOST = 'lovslhodt'
-#EMAIL_PORT = '25'
-#EMAIL_HOST_USER = 'matthewdwyer'
-#EMAIL_HOST_PASSWORD = 'test_password'
-#EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  # Example: Gmail SMTP server
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST')
+
+#SECURITY SETTINGS
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
