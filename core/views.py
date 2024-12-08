db884373471f8878740db6416539ed2fb4b22492
MattDwyer14 <mattyd799@gmail.com>
2024-12-08 00:23:07 +0000

    email backend

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
+from django.core.mail import send_mail
 from django.shortcuts import render, redirect
+import os
 from django.contrib import messages
 from .forms import contact_form
 from .models import HomePageContent
@@ -14,7 +16,21 @@ def contact(request):
     if request.method == 'POST':
         form = contact_form(request.POST)
         if form.is_valid():
-            form.save()
+            contact_message = form.save()
+
+            # Send email
+            subject = f"New Contact Form Submission: {contact_message.name}"
+            message = f"""
+            You have a new message from {contact_message.name} {contact_message.email}:
+
+            Reason: {contact_message.reason}
+            Message: {contact_message.message}
+            """
+            from_email = os.getenv('EMAIL_HOST_USER')
+            recipient_list = os.getenv('EMAIL_HOST_USER')
+
+            send_mail(subject, message, from_email, [recipient_list])
+
             messages.success(request, 'I\'ve recieved your message!')
             return redirect('contact')
     else:
